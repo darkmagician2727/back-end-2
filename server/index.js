909081e9b2912e02d4dd39bdea6683dcccb504e2
Thomas Albrecht <darkmagician2727@gmail.com>
2023-10-16 14:33:33 -0600

    initial commit

---
+++ server/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.listen(4000, () => console.log('Server is running on port 4000'))
