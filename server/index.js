fd6ac160e4b61e0571c0aca9dfc23654d394de68
Thomas Albrecht <darkmagician2727@gmail.com>
2023-10-16 16:00:59 -0600

    I forgor final commit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,22 @@
 const express = require('express');
+
 const cors = require('cors');
+
 const app = express();
+
+const houseController = require('./controller');
+
+
+
+
 app.use(express.json());
+
 app.use(cors());
-app.listen(4000, () => console.log('Server is running on port 4000'))
+
+app.get('/api/houses', houseController.getHouses);
+app.post('/api/houses', houseController.createHouse);
+app.put('/api/houses/:id', houseController.updateHouse);
+app.delete('/api/houses/:id', houseController.deleteHouse);
+
+
+app.listen(4004, () => console.log('Server is running on port 4004'))
